99f0a61028b413125823cecaf887a90f3849cebc
ganesha <valamital11@gmail.com>
2024-01-21 07:27:29 +1000

    Remove unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:welcome_screen_lottie_animation/screen/welcome_screen.dart';
 
 void main() {
   runApp(const MaterialApp(
-    /// You can attach a custom loading screen if you want to check custom loading functionality.
     home: WelcomeScreen(),
   ));
 }
